b3ebe079fb86bd803b053902a88fac12186cce5c
ashiera <ashiera@statwks.com>
2017-07-30 13:02:26 +0800

    Update getthreedaydata.sh

--- getthreedaydata.sh
+++ getthreedaydata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o data.json -k https://fc34cccf-10ad-40b5-a9f3-94bd430942ab:6ur2HB3eTq@twcservice.mybluemix.net:443/api/weather/v1/geocode/12.97/77.59/forecast/daily/3day.json
+curl -o data.json -k https://fc34cccf-10ad-40b5-a9f3-94bd430942ab:6ur2HB3eTq@twcservice.mybluemix.net:443/api/weather/v1/geocode/3.0920/101.7211/forecast/daily/3day.json
